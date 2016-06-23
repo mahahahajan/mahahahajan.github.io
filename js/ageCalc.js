cd9f173d19a5d88c6bbe7a027371f984dcceaa11
mahahahajan <Tiklum@PulkitApple.local>
2016-06-22 20:14:56 -0700

    hey new profile?

---
+++ js/ageCalc.js
@@ -0,0 +1,28 @@
+Site = (function(){
+  var dob = new Date("8 April 2000 04:00:00");
+  var internDate = new Date("1 April 2015 00:00:00");
+
+  setInterval(function(){
+    var now = new Date();
+    var duration = now - dob;
+    var years = duration / 31556900000;
+
+    var majorMinor = years.toFixed(9).toString().split('.');
+    $age.text(majorMinor[0] + "." + majorMinor[1]);
+  }, 100);
+
+  $(document).ready(function(){
+
+
+    $age = $('#age');
+
+    if(new Date() < internDate){
+      $('#starting').text(", starting on February 24");
+      $('#internstatus').text("will be");
+    }else{
+      $('#internstatus').text("am");
+    }
+
+
+  });
+}());

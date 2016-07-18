15828f257fa61d7bca86fde1aec1d41b35ede9ed
mahahahajan <Tiklum@PulkitApple.local>
2016-07-18 05:15:25 -0700

    Trying to make it more mobile friendly

--- js/ageCalc.js
+++ js/ageCalc.js
@@ -1,13 +1,13 @@
 Site = (function(){
   var dob = new Date("8 April 2000 04:00:00");
-  var internDate = new Date("1 April 2015 00:00:00");
+  var internDate = new Date("8 April 2015 00:00:00");
 
   setInterval(function(){
     var now = new Date();
     var duration = now - dob;
     var years = duration / 31556900000;
 
-    var majorMinor = years.toFixed(9).toString().split('.');
+    var majorMinor = years.toFixed(3).toString().split('.');
     $age.text(majorMinor[0] + "." + majorMinor[1]);
   }, 100);
 
@@ -17,7 +17,7 @@ Site = (function(){
     $age = $('#age');
 
     if(new Date() < internDate){
-      $('#starting').text(", starting on February 24");
+      $('#starting').text(", starting on April 8");
       $('#internstatus').text("will be");
     }else{
       $('#internstatus').text("am");

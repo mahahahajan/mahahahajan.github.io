bcd06effccc4fe0632205bf48b2aabdbf0d5ed0e
mahahahajan <pulkit8.mahajan@gmail.com>
2021-05-03 14:58:02 -0500

    Updated for AET class

--- main.js
+++ main.js
@@ -11,17 +11,24 @@ var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^
 matrix = matrix.split("");
 
 var font_size = 10;
-var columns = c.width/font_size; //number of columns for the rain
+var columns = c.width / font_size; //number of columns for the rain
 //an array of drops - one per column
 var drops = [];
 //x below is the x coordinate
 //1 = y co-ordinate of the drop(same for every drop initially)
-for(var x = 0; x < columns; x++)
-    drops[x] = 1; 
+for (var x = 0; x < columns; x++)
+    drops[x] = 1;
+
+
+//TODO: Hide reset button initially
+var resetButton = document.getElementById("reset");
+resetButton.style.display = "none";
+
+var matrixButton = document.getElementById("matrix");
+matrixButton.style.display = "flex";
 
 //drawing the characters
-function draw()
-{
+function draw() {
     //Black BG for the canvas
     //translucent BG to show trail
     ctx.fillStyle = "rgba(0, 0, 0, .04)";
@@ -30,16 +37,15 @@ function draw()
     ctx.fillStyle = "#00FF00";//green text
     ctx.font = font_size + "px arial";
     //looping over drops
-    for(var i = 0; i < drops.length; i++)
-    {
+    for (var i = 0; i < drops.length; i++) {
         //a random chinese character to print
-        var text = matrix[Math.floor(Math.random()*matrix.length)];
+        var text = matrix[Math.floor(Math.random() * matrix.length)];
         //x = i*font_size, y = value of drops[i]*font_size
-        ctx.fillText(text, i*font_size, drops[i]*font_size);
+        ctx.fillText(text, i * font_size, drops[i] * font_size);
 
         //sending the drop back to the top randomly after it has crossed the screen
         //adding a randomness to the reset to make the drops scattered on the Y axis
-        if(drops[i]*font_size > c.height && Math.random() > 0.975)
+        if (drops[i] * font_size > c.height && Math.random() > 0.975)
             drops[i] = 0;
 
         //incrementing Y coordinate
@@ -50,7 +56,27 @@ function draw()
 setInterval(draw, 35);
 
 
-function deadLink(){
-    alert('This app is currently unavailable'); 
+function deadLink() {
+    alert('This app is currently unavailable');
     return false;
+}
+function turnOnCanvas() {
+    c.style.width = "100%";
+    c.style.height = "100%";
+    var body = document.body;
+    body.style.backgroundColor = "black";
+    draw();
+    resetButton.style.display = "flex";
+    matrixButton.style.display = "none";
+}
+
+function turnOffCanvas() {
+    c.style.width = "0";
+    c.style.height = "0";
+    var body = document.body;
+    body.style.backgroundColor = "white";
+    resetButton.style.display = "none";
+    matrixButton.style.display = "flex";
+    
+    // draw();
 }
